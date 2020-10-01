73e280e09e9663911df307db719960e998c134db
Harshit <meharshitkapoor7@gmail.com>
2020-10-01 18:49:56 +0530

    Updated xml of interests

---
+++ app/src/main/java/com/example/learningeasle/PickInterests.java
@@ -0,0 +1,15 @@
+package com.example.learningeasle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class PickInterests extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick_interests);
+    }
+}
